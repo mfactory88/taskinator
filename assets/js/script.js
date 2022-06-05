234da3641be3485177fec3872609a3c3ba7387a0
MFact <mcfactory88@gmail.com>
2022-06-05 16:25:55 -0500

    output task name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,30 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.dir(taskTypeInput);
+
+    //create list item
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "This is a new task."
-    tasksToDoEl.appendChild(listItemEl);
     listItemEl.className = "task-item";
 
-    //console.log(event)
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    //give it a class name
+    taskInfoEl.className = "task-info"
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+    
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+
 }
 
 formEl.addEventListener("submit", createTaskHandler);
-
-console.log(createTaskHandler);
